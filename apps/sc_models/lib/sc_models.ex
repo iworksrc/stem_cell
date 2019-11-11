a982169fff408b60d3db06c5b338155d62bb031b
rke <imail.for.work@gmail.com>
2019-11-11 14:23:14 +0700

    add some models

--- apps/sc_models/lib/sc_models.ex
+++ apps/sc_models/lib/sc_models.ex
@@ -3,16 +3,39 @@ defmodule ScModels do
   Documentation for ScModels.
   """
 
+  alias ScModels.{FakeRepo, Item}
+
+  @repo FakeRepo
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ScModels.hello()
-      :world
-
+  get all items
   """
-  def hello do
-    :world
+  def list_items do
+    @repo.all(Item)
+  end
+
+  @doc """
+  get item by id
+  """
+  def get_item(id) do
+    @repo.get!(Item, id)
+  end
+
+  @doc """
+  get item by attributes
+  """
+
+  def get_item_by(attrs) do
+    @repo.get_by(Item, attrs)
+  end
+
+  @doc """
+  Structure for represent item entity
+  """
+  defmodule Item do
+    defstruct id: 0,
+              title: "default title",
+              description: "default description",
+              ends_at: ~N[2020-12-31 23:59:59]
   end
 end
