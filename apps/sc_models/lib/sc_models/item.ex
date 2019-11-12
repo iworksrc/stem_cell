74435e010c070df87efadcb3b0127c09d5384297
rke <imail.for.work@gmail.com>
2019-11-12 11:21:51 +0700

    refactor project structure

--- apps/sc_models/lib/sc_models/item.ex
+++ apps/sc_models/lib/sc_models/item.ex
@@ -1,6 +1,10 @@
-#defmodule ScModels.Item do
+defmodule ScModels.Item do
+  @moduledoc """
+  Structure for represent item entity
+  """
+  defstruct [:id, :title, :description, :ends_at]
 #  defstruct id: 0,
 #            title: "default title",
 #            description: "default description",
 #            ends_at: ~N[2020-12-31 23:59:59]
-#end
+end
