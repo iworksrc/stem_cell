a5b8081ff5080514d6299cbc311ad3def2cd0f65
rke <imail.for.work@gmail.com>
2019-12-01 15:47:03 +0700

    work versions of packages

--- apps/sc_web/mix.exs
+++ apps/sc_web/mix.exs
@@ -43,7 +43,9 @@ defmodule ScWeb.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ecto_sql, "~> 3.0", override: true},
+      {:postgrex, "~> 0.14.1"}
     ]
   end
 
